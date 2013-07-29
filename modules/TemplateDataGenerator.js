36e32578ff733324941ffd041df44928b2e7e027
Moriel Schottlender <mooeypoo@gmail.com>
2013-07-29 02:45:56 -0400

    Revert "Coding conventions"
    This reverts commit dc6b31a5a77943381a22ad003218bb02f0e4c3f6.

--- modules/TemplateDataGenerator.js
+++ modules/TemplateDataGenerator.js
@@ -1,8 +1,5 @@
-$( document ).ready( function ( $, mw ) {
-	'use strict';
-
-	var jsonTmplData, wikicontent,
-		textboxParts = [],
+$( document ).ready( function() {
+	var jsonTmplData, wikicontent, textboxParts = [],
 		selOpts = {
 			'undefined': mw.message( 'templatedatagenerator-modal-table-param-type-undefined' ),
 			'number': mw.message( 'templatedatagenerator-modal-table-param-type-number' ),
@@ -13,11 +10,8 @@ $( document ).ready( function ( $, mw ) {
 		rowCounter = 0,
 		modalBox = $( '.tdg-editscreen-modal-form' );
 
-	$( '.tdg-editscreen-main-button' ).click( function() {
-		var param, sel, $typeSel, $descText, $tbl, $delButton,
-			pAliases, $tSelect, pDesc, pDefault, reqChecked,
-			newTemplateData = false,
-			error = false;
+	$('.tdg-editscreen-main-button').click( function() {
+		var param, newTemplateData = false, error = false;
 
 		// Get the data from the textbox
 		wikicontent = $( '#wpTextbox1' ).val();
@@ -28,7 +22,6 @@ $( document ).ready( function ( $, mw ) {
 		// See if there was something between the tags:
 		if ( textboxParts && textboxParts[2] && textboxParts[2].trim().length > 0 ) {
 			textboxParts[2] = textboxParts[2].trim();
-
 			// Parse the json:
 			try {
 				jsonTmplData = $.parseJSON( textboxParts[2] );
@@ -45,38 +38,34 @@ $( document ).ready( function ( $, mw ) {
 
 		if ( !error ) {
 			// Create "type" selectbox:
-			$typeSel = $( '<select>' );
-
-			$typeSel.append( $( '<option>' ) );
-
-			for ( sel in selOpts ) {
-				$typeSel.append(
-					$( '<option>').prop( 'value', sel ).text( selOpts[ sel ] )
-				);
+			var typeSel = $( '<select>' );
+			typeSel.append( $( '<option>' ) );
+			for ( var sel in selOpts ) {
+				typeSel.append( $( '<option>').prop( 'value', sel ).text( selOpts[ sel ] ) );
 			}
 
 			// Description:
-			$descText = $( '<textarea>' )
-				.addClass( 'tdg-template-desc' );
-
+			var descText = $( '<textarea>', { 'class': 'tdg-template-desc' });
 			if ( !newTemplateData && jsonTmplData && jsonTmplData.description ) {
-				$descText.html( jsonTmplData.description );
+				descText.html( jsonTmplData.description );
 			}
 
 			// Param Table:
-			$tbl = $( '<table>').addClass( 'tdg-editTable' ).append( getRow( 'tdg-tr-head', [
-				{ 'text': mw.message( 'templatedatagenerator-modal-table-param-name' ) },
-				{ 'text': mw.message( 'templatedatagenerator-modal-table-param-aliases' ) },
-				{ 'text': mw.message( 'templatedatagenerator-modal-table-param-label' ) },
-				{ 'text': mw.message( 'templatedatagenerator-modal-table-param-desc' ) },
-				{ 'text': mw.message( 'templatedatagenerator-modal-table-param-type' ) },
-				{ 'text': mw.message( 'templatedatagenerator-modal-table-param-default' ) },
-				{ 'text': mw.message( 'templatedatagenerator-modal-table-param-required' ) },
-				{ 'text': mw.message( 'templatedatagenerator-modal-table-param-actions' ) }
-			] ) );
+			var tbl = $( '<table>').addClass( 'tdg-editTable' ).append( getRow( 'tdg-tr-head',
+				[
+					{ 'text': mw.message( 'templatedatagenerator-modal-table-param-name' ) },
+					{ 'text': mw.message( 'templatedatagenerator-modal-table-param-aliases' ) },
+					{ 'text': mw.message( 'templatedatagenerator-modal-table-param-label' ) },
+					{ 'text': mw.message( 'templatedatagenerator-modal-table-param-desc' ) },
+					{ 'text': mw.message( 'templatedatagenerator-modal-table-param-type' ) },
+					{ 'text': mw.message( 'templatedatagenerator-modal-table-param-default' ) },
+					{ 'text': mw.message( 'templatedatagenerator-modal-table-param-required' ) },
+					{ 'text': mw.message( 'templatedatagenerator-modal-table-param-actions' ) }
+				] ) );
 
 			// Add existing parameters:
 			if ( !newTemplateData && jsonTmplData && jsonTmplData.params ) {
+				var pAliases = '', tSelect;
 				for ( param in jsonTmplData.params ) {
 
 					// Set up the row:
@@ -94,11 +83,11 @@ $( document ).ready( function ( $, mw ) {
 					}
 
 					// Type:
-					$tSelect = $typeSel.clone().attr( 'id', 'tdc_pType_' + rowCounter );
+					tSelect = typeSel.clone().attr( 'id', 'tdc_pType_' + rowCounter );
 					if ( jsonTmplData.params[param].type ) {
-						$tSelect.val( jsonTmplData.params[param].type );
+						tSelect.val( jsonTmplData.params[param].type );
 					} else {
-						$tSelect.val( 0 );
+						tSelect.val( 0 );
 					}
 
 					pDefault = '';
@@ -106,99 +95,76 @@ $( document ).ready( function ( $, mw ) {
 						pDefault = jsonTmplData.params[param]['default'];
 					}
 
-					reqChecked = ( jsonTmplData.params[param].required !== undefined ) ?
-						jsonTmplData.params[param].required :
-						false;
+					reqChecked = ( jsonTmplData.params[param].required !== undefined ) ? jsonTmplData.params[param].required: false;
 
-					$delButton = $( '<button>' )
-						.attr( 'id', 'tdg_pButton_' + rowCounter )
-						.addClass( 'tdg-param-button-delete' )
-						.attr( 'data-paramnum', rowCounter )
-						.text( mw.message( 'templatedatagenerator-modal-button-delparam' ) )
-						.click( function() {
-							$( '.tdg-paramcount-' + $( this ).attr( 'data-paramnum' ) ).remove();
-						} );
+					var delButton = $( '<button>' ).attr( 'id', 'tdg_pButton_' + rowCounter ).addClass( 'tdg-param-button-delete' ).attr( 'data-paramnum', rowCounter ).text( mw.message( 'templatedatagenerator-modal-button-delparam' ) );
+					delButton.click( function() {
+						$( '.tdg-paramcount-' + $( this ).attr( 'data-paramnum' ) ).remove();
+					} );
 
 					// Add row:
-					$tbl.append( getRow( 'tdg-tr-param tdg-paramcount-' + rowCounter, [
+					tbl.append( getRow( 'tdg-tr-param tdg-paramcount-' + rowCounter, [
 						{ html: $( '<input>' ).attr( 'id', 'tdg_pName_' + rowCounter ).val( param ) },
 						{ html: $( '<input>' ).attr( 'id', 'tdg_pAliases_' + rowCounter ).val( pAliases ) },
 						{ html: $( '<input>' ).attr( 'id', 'tdg_pLabel_' + rowCounter ).val( jsonTmplData.params[param].label ) },
 						{ html: $( '<textarea>' ).attr( 'id', 'tdg_pDesc_' + rowCounter ).val( pDesc ) },
-						{ html: $tSelect },
+						{ html: tSelect },
 						{ html: $( '<input>' ).attr( 'id', 'tdg_pDefault_' + rowCounter ).val( pDefault ) },
 						{ html: $( '<input type="checkbox"/>' ).attr( 'id', 'tdg_pRequired' + rowCounter ).prop( 'checked', reqChecked ) },
-						{ html: $delButton }
+						{ html: delButton }
 					] ).attr( 'data-paramnum', rowCounter ) );
 					rowCounter++;
 				}
 			}
 
 			// "Add Param" button:
-			var addButton = $( '<button>' )
-				.attr( 'id', 'tdg_add_param')
-				.addClass( 'tdg-button-add-param' )
-				.text( mw.message( 'templatedatagenerator-modal-button-addparam' ) );
-
+			var addButton = $( '<button>' ).attr( 'id', 'tdg_add_param').addClass( 'tdg-button-add-param' ).text( mw.message( 'templatedatagenerator-modal-button-addparam' ) );
 			addButton.click( function() {
-				var $tSelect, $delButton;
-
 				//add empty row:
-				$delButton = $( '<button>' )
-					.attr( 'id', 'tdg_pButton_' + rowCounter )
-					.addClass( 'tdg-param-button-delete' )
-					.attr( 'data-paramnum', rowCounter )
-					.text( mw.message( 'templatedatagenerator-modal-button-delparam' ) );
-
-				$delButton.click( function() {
+				var delButton = $( '<button>' ).attr( 'id', 'tdg_pButton_' + rowCounter ).addClass( 'tdg-param-button-delete' ).attr( 'data-paramnum', rowCounter ).text( mw.message( 'templatedatagenerator-modal-button-delparam' ) );
+				delButton.click( function() {
 					$( '.tdg-paramcount-' + $( this ).attr( 'data-paramnum' ) ).remove();
 				} );
-
-				$tSelect = $typeSel.clone().attr('id', 'tdc_pType_' + rowCounter );
-				$tbl.append( getRow( 'tdg-tr-param tdg-paramcount-' + rowCounter, [
+				var tSelect = typeSel.clone().attr('id', 'tdc_pType_' + rowCounter );
+				tbl.append( getRow( 'tdg-tr-param tdg-paramcount-' + rowCounter, [
 					{ html: $( '<input>' ).attr( 'id', 'tdg_pName_' + rowCounter ) },
 					{ html: $( '<input>' ).attr( 'id', 'tdg_pAliases_' + rowCounter ) },
 					{ html: $( '<input>' ).attr( 'id', 'tdg_pLabel_' + rowCounter ) },
 					{ html: $( '<textarea>' ).attr( 'id', 'tdg_pDesc_' + rowCounter ) },
-					{ html: $tSelect },
+					{ html: tSelect },
 					{ html: $( '<input>' ).attr( 'id', 'tdg_pDefault_' + rowCounter ) },
 					{ html: $( '<input type="checkbox"/>' ).attr( 'id', 'tdg_pRequired' + rowCounter ) },
-					{ html: $delButton }
+					{ html: delButton }
 				] ).attr( 'data-paramnum', rowCounter ) );
-
 				rowCounter++;
 			} );
 
 			// Build the GUI
-			modalBox.append(
-				$( '<span>' ).addClass( 'tdg-title' ).text( mw.message( 'templatedatagenerator-modal-title-templatedesc' ) ),
-				$descText,
-				$( '<span>' ).addClass( 'tdg-title' ).text( mw.message( 'templatedatagenerator-modal-title-templateparams' ) ),
-				$tbl,
-				$addButton
-			);
+			modalBox
+				.append( $( '<span>' ).addClass( 'tdg-title' ).text( mw.message( 'templatedatagenerator-modal-title-templatedesc' ) ) )
+				.append( descText )
+				.append( $( '<span>' ).addClass( 'tdg-title' ).text( mw.message( 'templatedatagenerator-modal-title-templateparams' ) ) )
+				.append( tbl )
+				.append( addButton );
 
 			// Call the modal:
 			i18nModal( mw.message( 'templatedatagenerator-modal-buttons-apply' ), mw.message( 'templatedatagenerator-modal-buttons-cancel' ), modalBox );
 			modalBox.dialog( 'open' );
 		}
+
 	} );
 
 	/** Modal Setup **/
 	var i18nModal = function( btnApply, btnCancel, modalBox ) {
 		var modalButtons = {};
-
 		modalButtons[btnApply] = function() {
-			var jsonOut = {},
-				finalOutput = '';
-
+			var jsonOut = {};
 			// Description:
 			jsonOut.description = $( '.tdg-template-desc' ).val();
 			jsonOut.params = {};
 			// Go over the table:
 			$( '.tdg-editTable tr:gt(0)' ).each( function( index ) {
 				var trID = $( this ).attr( 'data-paramnum' );
-
 				jsonOut.params[ $( '#tdg_pName_' + trID ).val() ] = {
 					'label': $( '#tdg_pLabel_' + trID ).val(),
 					'type': $( '#tdg_pType_' + trID ).val(),
@@ -206,36 +172,28 @@ $( document ).ready( function ( $, mw ) {
 					'required': $( '#tdg_pRequired_' + trID ).val(),
 					'default': $( '#tdg_pDefault_' + trID ).val()
 				};
-
 				if ( $( '#tdg_pAliases_' + trID ).val() ) {
 					jsonOut.params[ $( '#tdg_pName_' + trID ).val() ].aliases = $( '#tdg_pAliases_' + trID ).val().split(",");
 				}
 			} );
 
 			// Now return jsonOut to the textbox:
+			var finalOutput = '';
 			if ( textboxParts && textboxParts[2] ) {
 				// put the json back where the tags were:
-				finalOutput = wikicontent.replace(
-					/(<templatedata>)([\s\S]*?)(<\/templatedata>)/i,
-					'<templatedata>\n' + JSON.stringify( jsonOut, null, '	' ) + '\n</templatedata>'
-				);
+				finalOutput = wikicontent.replace( /(<templatedata>)([\s\S]*?)(<\/templatedata>)/i, '<templatedata>\n' + JSON.stringify( jsonOut, null, '	' ) + '\n</templatedata>' );
 			} else {
 				// otherwise, put this at the end of the text:
-				finalOutput = wikicontent +
-					'\n<templatedata>\n' +
-					JSON.stringify( jsonOut, null, '\t' ) +
-					'\n</templatedata>';
+				finalOutput = wikicontent + '\n<templatedata>\n' + JSON.stringify( jsonOut, null, '	' ) + '\n</templatedata>';
 			}
-
 			$( '#wpTextbox1' ).val( finalOutput );
 			modalBox.dialog( 'close' );
 		};
-
 		modalButtons[btnCancel] = function() {
 			modalBox.dialog( 'close' );
 		};
 
-		modalBox.dialog( {
+		modalBox.dialog({
 			autoOpen: false,
 			height: window.innerHeight * 0.8,
 			width: window.innerWidth * 0.8,
@@ -246,17 +204,18 @@ $( document ).ready( function ( $, mw ) {
 				modalBox.empty();
 				rowCounter = 0;
 			}
-		} );
+		});
+
 	};
 
 	/** Methods **/
 	var getRow = function( trClass, tdObj ) {
-		var $row = $( '<tr>' ).addClass( trClass );
+		var row = $( '<tr>' ).addClass( trClass );
+		tdObj.forEach( function(td) {
+			row.append( $('<td>', td ) );
+		});
 
-		tdObj.forEach( function( td ) {
-			$row.append( $( '<td>', td ) );
-		} );
-
-		return $row;
+		return row;
 	};
-}( jQuery, mediaWiki ) );
+
+});
